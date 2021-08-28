9db0c311017942b30f91a828e88b145fb2c14cb9
NuttaponN <ponidc@gmail.com>
2021-08-28 16:43:23 +0700

    Tiny Brach F

--- index.js
+++ index.js
@@ -129,6 +129,23 @@ const showReadableTargetTimeTH = (targetTime) => {
 	return result;
 };
 
+var parseDateToObject = (date) => {
+		const datePattern = /^(\d{2})\/(\d{2})\/(\d{4})$/;
+		const [, day, month, year] = datePattern.exec(date);
+		return {
+			day: parseInt(day),
+			month: parseInt(month),
+			year: parseInt(year),
+		};
+	};
+
+	var dateToDMY = (date) => {
+		var d = date.getDate();
+		var m = date.getMonth() + 1; //Month from 0 to 11
+		var y = date.getFullYear();
+		return "" + (d <= 9 ? "0" + d : d) + "/" + (m <= 9 ? "0" + m : m) + "/" + y;
+	};
+
 exports.lineNotify = lineNotify;
 exports.thaiNumber = thaiNumber;
 exports.showTargetTime = showTargetTime;
